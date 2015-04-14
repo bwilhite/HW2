66a5720b92288c09fb1e0cf31326a0b78d632a05
Wilhite <brighid.wilhite@gmail.com>
2015-04-13 22:02:25 -0700

    ergh

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+
 Rottenpotatoes::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-  root to: => redirect('/movies')
+  resources :movies
+  root :to => redirect('/movies')
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -12,7 +14,7 @@ Rottenpotatoes::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :movies
+
 
   # Sample resource route with options:
   #   resources :products do
